a2cc1712a1cdf60d4b39cd4da28f278f9ce1dd61
lxst <lxst@YEGOR>
2025-10-06 20:39:36 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Check(userInput));
+            Console.ReadKey();
+        }
+
+        static bool Check(string userInput)
+        {
+            Stack<char> userSymbols = new Stack<char>();
+            Dictionary<char, char> bracket = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
+
+            foreach (char c in userInput)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    userSymbols.Push(bracket[c]);
+                }
+
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (userSymbols.Count == 0 || userSymbols.Pop() != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (userSymbols.Count == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
